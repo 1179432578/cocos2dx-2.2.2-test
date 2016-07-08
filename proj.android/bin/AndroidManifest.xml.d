132d029e91e9a2ccd064cd7bba84b1feb1b6bf3b
Lu Fei <lewis.lu@mochang.net>
2016-07-08 18:38:09 +0800

    add android support

--- proj.android/bin/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-/Users/lufei1/cocos2d-x-2.2.2-pak/projects/cocos2dx-2.2.2-test/proj.android/bin/AndroidManifest.xml : \
-/Users/lufei1/cocos2d-x-2.2.2-pak/projects/cocos2dx-2.2.2-test/proj.android/AndroidManifest.xml \
--- proj.android/bin/HelloWorld-debug-unaligned.apk.d
+++
@@ -1,5 +0,0 @@
-/Users/lufei1/cocos2d-x-2.2.2-pak/projects/cocos2dx-2.2.2-test/proj.android/bin/HelloWorld-debug-unaligned.apk : \
-/Users/lufei1/cocos2d-x-2.2.2-pak/projects/cocos2dx-2.2.2-test/proj.android/bin/HelloWorld.ap_ \
-/Users/lufei1/cocos2d-x-2.2.2-pak/projects/cocos2dx-2.2.2-test/proj.android/bin/classes.dex \
-/Users/lufei1/android/adt/sdk/tools/support/annotations.jar \
-/Users/lufei1/cocos2d-x-2.2.2-pak/projects/cocos2dx-2.2.2-test/proj.android/libs/armeabi/libhellocpp.so \
